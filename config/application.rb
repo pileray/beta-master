4829614e2a3ce6a3a49f79f13084cd2dbb00d780
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-06 18:00:33 +0900

    Add: helper等の自動作成を解除

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,16 @@ module BetaMaster
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+
+    # assets,helper,specファイルの自動作成を解除
+    config.generators do |g|
+      g.assets false
+      g.helper     false
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
