75d5dbddd929287ef439cc23d17f2f779f711a45
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-10 15:54:27 +0900

    Add: sorcery gemを導入しUserモデルを作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
