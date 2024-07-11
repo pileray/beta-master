137c021b33d03d93ebde2b352fdae78a46d07ffc
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-12 02:14:00 +0900

    Add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,17 @@ Rails.application.routes.draw do
   root "static_pages#top"
   get "/complete_signup", to: "static_pages#complete_signup"
   delete '/logout', to: 'sessions#destroy'
+
+  resources :quizzes, only: [:index] do
+    # 今後クイズ出題機能で実装
+    resource 'random_exam', on: :member, module: 'quizzes', only: %w[show]
+    resource 'bookmarked_exam', on: :member, module: 'quizzes', only: %w[show]
+  end
+
+  # 今後クイズ出題機能で実装
+  namespace 'quizzes' do
+    delete 'random_exam', to: 'random_exams#destroy'
+    delete 'bookmarked_exam', to: 'bookmarked_exams#destroy'
+  end
+
 end
