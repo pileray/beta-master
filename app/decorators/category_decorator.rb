22c004cc4058ed3cd2176a3469f90b9641321a7a
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-16 15:42:07 +0900

    Fix: quiz decoratorをcategory decoratorに命名を修正しパーシャル化

---
+++ app/decorators/category_decorator.rb
@@ -0,0 +1,24 @@
+class CategoryDecorator < Draper::Decorator
+  delegate_all
+  def category_badge_class
+    case self.category.name
+    when 'エンタメ'
+      'badge bg-sky-100 text-xs'
+    when 'スポーツ'
+      'badge bg-red-300 text-xs'
+    when '生活'
+      'badge bg-yellow-300 text-xs'
+    when '社会'
+      'badge bg-orange-300 text-xs'
+    when '文学・歴史'
+      'badge bg-blue-300 text-xs'
+    when '自然科学'
+      'badge bg-purple-300 text-xs'
+    when '芸術・音楽'
+      'badge bg-green-500 text-xs'
+    else
+      'badge badge-outline text-xs'
+    end
+  end
+
+end
