22182b6c70c968435f34c9dadc343817dfed3e49
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-27 07:43:20 +0900

    Add: 問題文の全文表示, 5文字追加表示機能を追加

--- app/javascript/controllers/random_exam_controller.js
+++ app/javascript/controllers/random_exam_controller.js
@@ -2,11 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="random-exam"
 export default class extends Controller {
-  static values = { answer: String }
-  static targets = [ "answer" ]
+  static values = { answer: String, body: String, initialLength: Number }
+  static targets = [ "answer", "body" ]
+
+  connect() {
+    this.currentLength = this.initialLengthValue - 1;
+    this.fullLength = this.bodyValue.length;
+  }
 
   showAnswer() {
-    this.answerTarget.textContent = this.answerValue
-    this.answerTarget.classList.remove("text-blue-400")
+    this.answerTarget.textContent = this.answerValue;
+    this.answerTarget.classList.remove("text-blue-400");
+  }
+
+  showBody() {
+    this.bodyTarget.textContent = this.bodyValue;
+  }
+
+  addCharacters() {
+    if (this.currentLength < this.fullLength) {
+      this.currentLength = Math.min(this.currentLength + 5, this.fullLength);
+      this.updateBodyText();
+    }
+  }
+
+  updateBodyText() {
+    const displayedText = this.bodyValue.slice(0, this.currentLength);
+    const suffix = (this.currentLength < this.fullLength) ? "/" : "";
+    this.bodyTarget.textContent = displayedText + suffix;
   }
 }
