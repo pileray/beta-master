c2fb70012df16ce5964abe5932318bd67790eae4
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-29 17:51:42 +0900

    Add: ランダム出題終了リンクを追加

--- app/controllers/quizzes/random_exams_controller.rb
+++ app/controllers/quizzes/random_exams_controller.rb
@@ -3,4 +3,9 @@ class Quizzes::RandomExamsController < ApplicationController
     @quiz = Quiz.find(params[:quiz_id])
     @next_quiz = Quiz.get_random_quiz(session)
   end
+
+  def destroy
+    session[:answered_quiz_ids] = []
+    redirect_to root_path, status: :see_other
+  end
 end
