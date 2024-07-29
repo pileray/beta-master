4ebe92545637732f80f7ce3e980e16126d8abb4f
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-29 15:47:48 +0900

    Add: 冒頭文字を1文字ずつ表示するよう変更

--- app/decorators/quiz_decorator.rb
+++ app/decorators/quiz_decorator.rb
@@ -2,6 +2,6 @@ class QuizDecorator < Draper::Decorator
   delegate_all
 
   def truncated_body
-    object.body[0, object.offset] + "/"
+    object.body[0, object.offset]
   end
 end
--- app/javascript/controllers/random_exam_controller.js
+++ app/javascript/controllers/random_exam_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="random-exam"
 export default class extends Controller {
-  static values = { answer: String, body: String, initialLength: Number }
+  static values = { answer: String, body: String, initialBody: String }
   static targets = [ "answer", "body" ]
 
   connect() {
-    this.currentLength = this.initialLengthValue - 1;
+    this.currentLength = 0;
     this.fullLength = this.bodyValue.length;
+    this.initialText = this.initialBodyValue;
+    this.fullText = this.bodyValue;
+    this.bodyTarget.textContent = "";
+    this.typeWriterEffect();
   }
 
   showAnswer() {
@@ -17,7 +21,7 @@ export default class extends Controller {
 
   showBody() {
     this.currentLength = this.fullLength;
-    this.bodyTarget.textContent = this.bodyValue;
+    this.bodyTarget.textContent = this.fullText;
   }
 
   addCharacters() {
@@ -28,8 +32,19 @@ export default class extends Controller {
   }
 
   updateBodyText() {
-    const displayedText = this.bodyValue.slice(0, this.currentLength);
+    const displayedText = this.fullText.slice(0, this.currentLength);
     const suffix = (this.currentLength < this.fullLength) ? "/" : "";
     this.bodyTarget.textContent = displayedText + suffix;
+  }
+
+  typeWriterEffect() {
+    this.interval = setInterval(() => {
+      if (this.currentLength < this.initialText.length) {
+        this.currentLength += 1;
+        this.bodyTarget.textContent = this.initialText.slice(0, this.currentLength) + "/";
+      } else {
+        clearInterval(this.interval);
+      }
+    }, 100);
   }
 }
