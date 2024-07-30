e91ff949c7acb0b2c39095fef2727fbe03e393b8
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-31 01:55:07 +0900

    Fix: rubocop, erblintによる構文チェック

--- app/controllers/quizzes/bookmarks_controller.rb
+++ app/controllers/quizzes/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class Quizzes::BookmarksController < ApplicationController
   def create
     @quiz = Quiz.find(params[:quiz_id])
     current_user.bookmark(@quiz)
-    flash.now[:success] = "お気に入りに追加しました"
+    flash.now[:success] = 'お気に入りに追加しました'
   end
 
   def destroy
     @quiz = Quiz.find(params[:quiz_id])
     current_user.unbookmark(@quiz)
-    flash.now[:success] = "お気に入りから解除しました"
+    flash.now[:success] = 'お気に入りから解除しました'
   end
 end
