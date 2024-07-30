102d65cffa95127226333d2d0d348072c74d905f
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-07-31 01:49:50 +0900

    Fix: お気に入り登録時にflashを表示させるよう修正

--- app/controllers/quizzes/bookmarks_controller.rb
+++ app/controllers/quizzes/bookmarks_controller.rb
@@ -2,10 +2,12 @@ class Quizzes::BookmarksController < ApplicationController
   def create
     @quiz = Quiz.find(params[:quiz_id])
     current_user.bookmark(@quiz)
+    flash.now[:success] = "お気に入りに追加しました"
   end
 
   def destroy
     @quiz = Quiz.find(params[:quiz_id])
     current_user.unbookmark(@quiz)
+    flash.now[:success] = "お気に入りから解除しました"
   end
 end
