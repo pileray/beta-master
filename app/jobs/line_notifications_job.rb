55d5fc6749db4d1b554cd812edf0bdf72bb0189e
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-01 02:34:51 +0900

    Add: LINE通知のjobを追加し、sidekiqで定期実行設定

---
+++ app/jobs/line_notifications_job.rb
@@ -0,0 +1,30 @@
+require 'line/bot'
+
+class LineNotificationsJob < ApplicationJob
+  queue_as :default
+  sidekiq_options retry: 3
+
+  def perform(*args)
+    users = User.where(line_notification: true)
+
+    users.each do |user|
+      @quiz = Quiz.get_notification_quiz(user)
+
+      message_text = "【今日のクイズ🌤】\n\nおはようございます！本日のクイズです！この読ませ押し、分かりますか？👀\n\n#{@quiz.decorate.truncated_body}/\n\n回答はコチラ💡\n\nhttps://a4c1-220-147-214-245.ngrok-free.app/quizzes/#{@quiz.id}/random_exam?openExternalBrowser=1"
+
+      message = { type: 'text', text: message_text }
+      uid = user.authentications.where(provider: 'line').take.uid
+      response = client.push_message(uid, message)
+      logger.info "PushLineSuccess"
+    end
+  end
+
+  private
+  def client
+    Line::Bot::Client.new { |config|
+      config.channel_id = Rails.application.credentials.line_message.channel_id
+      config.channel_secret = Rails.application.credentials.line_message.secret_key
+      config.channel_token = Rails.application.credentials.line_message.access_token
+    }
+  end
+end
