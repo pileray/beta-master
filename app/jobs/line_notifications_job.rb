dc345af2360b6d26f58cbed43d86f5ee51bef07a
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-02 01:12:40 +0900

    Fix: LINE通知文をヒアドキュメント化

--- app/jobs/line_notifications_job.rb
+++ app/jobs/line_notifications_job.rb
@@ -4,27 +4,40 @@ class LineNotificationsJob < ApplicationJob
   queue_as :default
   sidekiq_options retry: 3
 
-  def perform(*args)
+  # rubocop:disable Metrics/MethodLength
+  def perform(*_args)
     users = User.where(line_notification: true)
 
     users.each do |user|
       @quiz = Quiz.get_notification_quiz(user)
 
-      message_text = "【今日のクイズ🌤】\n\nおはようございます！本日のクイズです！この読ませ押し、分かりますか？👀\n\n#{@quiz.decorate.truncated_body}/\n\n回答はコチラ💡\n\nhttps://#{Settings.default_url_options.host}/quizzes/#{@quiz.id}/random_exam?openExternalBrowser=1"
+      message_text = <<~TEXT.chomp
+      【今日のクイズ🌤】
+      
+      #{@quiz.decorate.truncated_body}/
+
+        おはようございます！本日のクイズです！この読ませ押し、分かりますか？👀
+
+        回答はコチラ💡
+
+        https://#{Settings.default_url_options.host}/quizzes/#{@quiz.id}/random_exam?openExternalBrowser=1
+      TEXT
 
       message = { type: 'text', text: message_text }
-      uid = user.authentications.where(provider: 'line').take.uid
-      response = client.push_message(uid, message)
-      logger.info "PushLineSuccess"
+      uid = user.authentications.find_by(provider: 'line').uid
+      client.push_message(uid, message)
+      logger.info 'PushLineSuccess'
     end
   end
+  # rubocop:enable Metrics/MethodLength
 
   private
+
   def client
-    Line::Bot::Client.new { |config|
+    Line::Bot::Client.new do |config|
       config.channel_id = Rails.application.credentials.line_message.channel_id
       config.channel_secret = Rails.application.credentials.line_message.secret_key
       config.channel_token = Rails.application.credentials.line_message.access_token
-    }
+    end
   end
 end
