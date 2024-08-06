b5919d6bd425533279263f9bb0d2634f4b23bdfe
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-06 19:34:06 +0900

    Add: 通知設定タブを追加

--- app/controllers/mypage/line_notifications_controller.rb
+++ app/controllers/mypage/line_notifications_controller.rb
@@ -1,10 +1,25 @@
 class Mypage::LineNotificationsController < Mypage::BaseController
+  before_action :set_user
+
   def edit
   end
 
   def update
+    @user.update!(line_notification_params)
+    flash.now[:success] = if @user.line_notification
+                            '通知設定をオンにしました'
+                          else
+                            '通知設定をオフにしました'
+                          end
   end
 
-  def destroy
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def line_notification_params
+    params.require(:user).permit(:line_notification)
   end
 end
