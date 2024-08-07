6667964443160e66c514ff7309be770d42010816
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-07 16:50:14 +0900

    Add: リクエストスペックの追加

--- spec/requests/mypage/accounts_spec.rb
+++ spec/requests/mypage/accounts_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Mypage::Quizzes", type: :request do
+RSpec.describe "Mypage::Accounts", type: :request do
   let!(:user) { create(:user) }
   let!(:quiz1) { create(:quiz, body: '問題1の問題文です', answer: '回答1') }
   let!(:quiz2) { create(:quiz, body: '問題2の問題文です', answer: '回答2') }
   before do
     login_user_request_spec(user)
-    user.bookmarked_quizzes << quiz1
   end
 
-  describe "GET /index" do
-    it "マイクイズ帳ページが正常に表示され、マイクイズが表示される" do
-      get mypage_quizzes_path
+  describe "GET /edit" do
+    it "ユーザー退会ページが正常に表示される" do
+      get edit_mypage_account_path
       expect(response).to have_http_status(200)
-      expect(response.body).to include('問題1')
-      expect(response.body).not_to include('問題2')
+      expect(response.body).to include("ユーザー退会")
     end
   end
 
--- spec/requests/mypage/line_notifications_spec.rb
+++ spec/requests/mypage/line_notifications_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Mypage::LineNotifications", type: :request do
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/mypage/line_notifications/edit"
-      expect(response).to have_http_status(:success)
+  let!(:user) { create(:user) }
+  before do
+    login_user_request_spec(user)
+  end
+
+  describe "GET mypage/line_notification/edit" do
+    it "通知設定ページが正常に表示される" do
+      get edit_mypage_line_notification_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("LINE通知設定")
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/mypage/line_notifications/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/mypage/line_notifications/destroy"
-      expect(response).to have_http_status(:success)
+  describe "PATCH mypage/line_notification" do
+    it "通知設定が更新されること" do
+      patch mypage_line_notification_path,
+            params: { user: { line_notification: false } },
+            headers: { 'Accept' => 'text/vnd.turbo-stream.html' }
+      expect(response).to have_http_status(200)
+      expect(user.reload.line_notification).to eq(false)
     end
   end
 
