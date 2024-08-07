6da1ef3a50b04c2590bb94730be6030a8258ae9d
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-07 16:50:47 +0900

    Fix: 全てのスペックをファクトリーを使用する形で修正

--- spec/system/random_exams_spec.rb
+++ spec/system/random_exams_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "RandomExams", type: :system do
-  let!(:quiz1) { Quiz.find_by!(body: '問題1の問題文です') }
-  let!(:quiz2) { Quiz.find_by!(body: '問題2の問題文です') }
-  let!(:quiz3) { Quiz.find_by!(body: '問題3の問題文です') }
+  let!(:quiz1) { create(:quiz, body: '問題1の問題文です', answer: '回答1') }
 
   describe '問題が表示され、回答と問題文追加ができる' do
     it '問題文が表示され、回答と問題文追加ができる' do
       visit quiz_random_exam_path(quiz1)
       sleep 0.5
 
-      expect(page).to have_content('問題')
+      expect(page).to have_content('問題1')
       expect(page).to have_content('回答表示')
 
       click_button '5文字追加'
-      expect(page).to have_content('問題1の問題文')
+      expect(page).to have_content('問題1の問題文で')
 
       click_button '全文表示'
       expect(page).to have_content('問題1の問題文です')
