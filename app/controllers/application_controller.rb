79e235d06da61ff2d66ce40f6f947feb0ba78fac
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-13 16:54:24 +0900

    Fix: rubocop, erblintによる構文修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :error
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+  rescue_from ActionController::RoutingError, with: :render404
 
-  rescue_from StandardError, with: :render_500
+  rescue_from StandardError, with: :render500
 
-  def render_404
-    render template: 'errors/not_found', status: 404
+  def render404
+    render template: 'errors/not_found', status: :not_found
   end
 
-  def render_500
-    render template: 'errors/internal_server_error', status: 500
+  def render500
+    render template: 'errors/internal_server_error', status: :internal_server_error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %w[destroy]
 
-  match '*path', to: 'application#render_404', via: :all
+  match '*path', to: 'application#render404', via: :all
 end
