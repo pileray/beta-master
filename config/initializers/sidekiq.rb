225d84829f80ca7601b50542cb69f94fcc33c649
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-15 19:26:08 +0900

    Fix: sidekiq用のredisの設定管理をconfig gemから環境変数に切り替え

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Settings.redis.url }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Settings.redis.url }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
