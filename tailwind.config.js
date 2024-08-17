f8e64a759b2fa2012f11975ec6e74d5208200140
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-08-17 13:00:57 +0900

    Fix: ECSコンテナ起動時のエラーに対処

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/**/*.html.erb',
+    './app/views/**/*.erb'
+  ],
+  safelist: [
+    'bg-sky-100',
+    'bg-red-300',
+    'bg-yellow-300',
+    'bg-orange-300',
+    'bg-blue-300',
+    'bg-purple-300',
+    'bg-green-500',
+    'text-xs',
+    'badge',
+    'badge-outline',
+    'toast',
+    'toast-end',
+    'alert',
+    'alert-success',
+    'alert-error',
+    'alert-info',
+    'btn-bookmark',
+    'btn-unbookmark'
   ],
   plugins: [
     require('daisyui')
@@ -11,33 +33,5 @@ module.exports = {
   daisyui: {
     themes: ["light", "lemonade", "cupcake"],
     darkTheme: false,
-  },
-  purge: {
-    content: [
-      './app/**/*.html.erb',
-      './app/helpers/**/*.rb',
-      './app/javascript/**/*.js',
-      './app/views/**/*.erb',
-    ],
-    safelist: [
-      'bg-sky-100',
-      'bg-red-300',
-      'bg-yellow-300',
-      'bg-orange-300',
-      'bg-blue-300',
-      'bg-purple-300',
-      'bg-green-500',
-      'text-xs',
-      'badge',
-      'badge-outline',
-      'toast',
-      'toast-end',
-      'alert',
-      'alert-success',
-      'alert-error',
-      'alert-info',
-      'btn-bookmark',
-      'btn-unbookmark'
-    ],
-  },
+  }
 }
